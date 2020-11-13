711ccca7a5927a404e6cf9031e0e64a9180e9059
Siina Kaakinen <m2936@student.jamk.fi>
2020-11-13 15:12:09 +0200

    unicafe step 5 tehty

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+const Statistics = ({good, neutral, bad}) => {
+
+  const total = good + bad + neutral
+  const average = ((good * 1) + (bad * -1) + (neutral * 0))/(good + bad + neutral)
+  const positive = (good/(good + bad + neutral))*100
+
+  if(total > 0) {
+  return (
+    <div>
+      <h2>Tulokset</h2>
+      <StatisticLine text="Hyvä palaute: " value ={good} />
+      <StatisticLine text="Neutraali palaute: " value ={neutral} />
+      <StatisticLine text="Huono palaute: " value ={bad} />
+      <StatisticLine text="Palaute yhteensä: " value ={total} />
+      <StatisticLine text="Palautteen keskiarvo: " value ={average} />
+      <StatisticLine text="Positiivisen palautteen määrä: " value ={positive} />
+    </div>
+  )
+  }
+  return (
+    <div>
+      <p>Palautetta ei ole vielä annettu</p>
+    </div>)
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const StatisticLine = ({text, value}) => {
+
+  return(
+    <div>
+      <p>{text} {value}</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const addGood = () => setGood(good + 1)
+  const addNeutral = () => setNeutral(neutral + 1)
+  const addBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>Anna palautetta</h1>
+      <Button onClick={addGood} text='Hyvä' />
+      <Button onClick={addNeutral} text='Neutraali' />
+      <Button onClick={addBad} text='Huono' />
+      
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
