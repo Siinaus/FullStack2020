505aa43a4590fcc5ca73af12f4b69b70df232c7e
Siinaus <55137603+Siinaus@users.noreply.github.com>
2021-01-04 15:06:20 +0200

    3c

---
+++ osa3/puhelinluettelo-backend/models/person.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const url = process.env.MONGODB_URI
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log('Yhdistetty MongoDB'))
+  .catch((error) =>
+    console.log(`Virhe yhdistäessä MongoDB: ${error.message}`)
+  )
+
+const personSchema = new mongoose.Schema({
+  name: { type: String, minlength: 3, required: true, unique: true },
+  number: { type: String, minlength: 8, required: true, unique: false }
+})
+
+personSchema.plugin(uniqueValidator)
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    const object = returnedObject
+    object.id = object._id.toString()
+    delete object._id
+    delete object.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
